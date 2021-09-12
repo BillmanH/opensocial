797e925ae4f87547a2ef4b9bbd8db89f029706ae
billmanh <william.jeffrey.harding@gmail.com>
2021-09-12 15:30:36 -0700

    uploading pictures

--- scripts/upload_pictures.py
+++ scripts/upload_pictures.py
@@ -1,15 +1,42 @@
 #%%
 import pandas as pd
+import numpy as np
 import yaml
 from azure.storage.blob import BlobClient
 
-params=yaml.safe_load(open("../blob_keys.yaml"))
-blob = BlobClient.from_connection_string(conn_str=params["conn_str"],
-                                        container_name=params["container_name"],
-                                        blob_name=params["blob_name"])
+acceptable_image_types = ["png", "jpg"]
 
-with open("../data/test.txt", "rb") as data:
-    blob.upload_blob(data)
+params = yaml.safe_load(open("../blob_keys.yaml"))
 
 
-blob.get
+def get_blob(name):
+    blob = BlobClient.from_connection_string(
+        conn_str=params["conn_str"],
+        container_name=params["container_name"],
+        blob_name=name,
+    )
+    return blob
+
+def clean_tags(pic):
+    acceptable_keys = [k for k in list(pic.keys()) if k not in ["Unnamed: 0","fullpath","isdir","size"]]
+    clean_pic = {k: str(pic[k].replace('\\','+')) for k in acceptable_keys}
+    return clean_pic
+
+
+df = pd.read_csv("../data/social_files.csv")
+df = df[df["extension"].isin(acceptable_image_types)].dropna()
+pictures = df.to_dict(orient="records")
+print(f'uploading {len(pictures)} documents to {params["container_name"]}')
+
+unable_to_upload = []
+for i, pic in enumerate(pictures):
+    if i % 100 == 0:
+        print(np.round((i / len(pictures) * 100), decimals=4), "% completed")
+    get_blob(pic.get("item")).upload_blob(
+        open(pic.get("fullpath"), "rb"),
+        tags=clean_tags(pic),
+        overwrite=params["overwrite_blob"],
+    )
+
+
+pd.DataFrame(unable_to_upload).to_csv("../data/unable_to_upload.csv")
